39e03d3aa057dbf68717742507bfadab4ac97f5c
andreamsgi27 <andreamsgi27@hotmail.com>
2024-10-17 05:09:44 -0700

    clase items iniciada, esqueleto hecho

---
+++ game/src/main/java/org/factoriaf5/game/Items.java
@@ -0,0 +1,58 @@
+package org.factoriaf5.game;
+
+public class Items {
+    private String itemName;
+
+    public Items(String itemName) {
+        this.itemName = itemName;
+        }
+        public String getItemName() {
+            return itemName;
+        }
+
+
+    public int Sword (int aidenDamage) {
+        itemName = "Espada";
+        int swordDamage = 10;
+        int totalDamage  = swordDamage + aidenDamage;
+        System.out.println("Aiden usa una espada con" + swordDamage + " de daño.");
+        System.out.println("Aiden inflige ahora" + totalDamage + " de daño");
+        return totalDamage;
+    }
+    public int Potion (int aidenHealth) {
+        itemName = "Poción";
+        int potion = 20;
+        int totalHealth = aidenHealth + potion;
+        System.out.println("Aiden usa una poción con" + potion + " de vida");
+        System.out.println("Aiden tiene ahora" + totalHealth + " de vida");
+        return  totalHealth;
+    }
+
+    public void garlic(){
+        itemName = "Ajo";
+        if (Monster.getMonsterName().equals("Vampiro") && Monster.getLifeStealing() == true){
+            Monster.setLifeStealing(false);
+        } else {
+            System.out.println("El ajo no puede usarse");
+        }
+    }
+
+    public void glasses(){
+        if (Monster.getMonsterName().equals("Fantasma") && Monster.getInvisibility() == true){
+            Monster.setInvisibility(false);
+        } else {
+            System.out.println("Las gafas no pueden usarse");
+        }
+    }
+
+    public void whistle(){
+        itemName = "Silbato";
+        if (Monster.getMonsterName().equals("Esqueleto") && Monster.getNumSkeletons() >=2){
+            Monster.setNumSkeletons = 1;
+        }
+    }
+
+
+
+}
+
