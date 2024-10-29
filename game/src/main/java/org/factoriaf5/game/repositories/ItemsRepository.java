55df3e4f33f2f13203866854ac15a96bb486f8ca
andreamsgi27 <andreamsgi27@hotmail.com>
2024-10-29 11:28:34 +0100

    items repository

--- game/src/main/java/org/factoriaf5/game/repositories/ItemsRepository.java
+++ game/src/main/java/org/factoriaf5/game/repositories/ItemsRepository.java
@@ -1,4 +1,6 @@
 package org.factoriaf5.game.repositories;
-public class ItemsRepository {
-    
+import org.factoriaf5.game.models.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemsRepository extends JpaRepository<Items, Long> {
 }
