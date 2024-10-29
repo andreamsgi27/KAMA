8d713c4261dc474476f182e4241d3cb721d07515
andreamsgi27 <andreamsgi27@hotmail.com>
2024-10-29 11:26:43 +0100

    items service

--- game/src/main/java/org/factoriaf5/game/services/ItemsService.java
+++ game/src/main/java/org/factoriaf5/game/services/ItemsService.java
@@ -1,5 +1,21 @@
 package org.factoriaf5.game.services;
 
+import org.factoriaf5.game.Items;
+import org.factoriaf5.game.repositories.ItemsRepository;
+import org.hibernate.mapping.List;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ItemsService {
-    
+    ItemsRepository repository;
+
+    public ItemsService(ItemsRepository repository){
+        this.repository = repository;
+    }
+
+    public List<Items> getAllItems(){
+        List<Items> items = repository.findAll()
+        return items;
+    }
+
 }
