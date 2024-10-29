cad9305e8675f0b7bd4c659fab44d0c0e6c8d3cf
andreamsgi27 <andreamsgi27@hotmail.com>
2024-10-29 10:37:55 +0100

    item model

--- game/src/main/java/org/factoriaf5/game/models/Items.java
+++ game/src/main/java/org/factoriaf5/game/models/Items.java
@@ -1,4 +1,41 @@
 package org.factoriaf5.game.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "items")
 public class Items {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_items")
+    private Long id;
+    private String name;
+    private String description;
+
+    public Items(Long id, String name, String description){
+        this.id =  id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+        }
     
+    public String getDescription(){
+        return description;
+    }
+
+
+
 }
