1e35292ceaa85a72e13733919c1f7ce381ae32bf
andreamsgi27 <andreamsgi27@hotmail.com>
2024-10-29 10:53:21 +0100

    items controller

--- game/src/main/java/org/factoriaf5/game/controllers/ItemsController.java
+++ game/src/main/java/org/factoriaf5/game/controllers/ItemsController.java
@@ -1,10 +1,21 @@
 package org.factoriaf5.game.controllers;
-<<<<<<< HEAD
 import org.factoriaf5.game.models.*;
+import org.factoriaf5.game.services.ItemsService;
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
+import org.hibernate.mapping.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ItemsController{
-=======
+    private ItemsService services;
 
-public class ItemsController {
->>>>>>> origin/aiden
+    public ItemsController(ItemsService services){
+        this.services = services;
+    }
 
+    @GetMapping(path = "/items")
+    public List<Item> getItems(){
+        return services.getItems();
+        }
 }
