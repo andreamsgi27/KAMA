8a514f4adb0746f337d1a26f0272b1a6c0f51403
mercyluz <mercyluz@hotmail.com>
2024-10-29 15:02:52 +0100

    crando controllers, model, service y repositories de aiden

--- src/main/java/org/factoriaf5/game/controllers/AidenController.java
+++ src/main/java/org/factoriaf5/game/controllers/AidenController.java
@@ -1,5 +1,18 @@
-package org.factoriaf5.game;
+package org.factoriaf5.game.controllers;
 
+import java.util.List;
+
+import org.factoriaf5.game.models.Aiden;
+import org.factoriaf5.game.services.AidenService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AidenController {
+    private final AidenService AidenService;
 
+    public AidenController(AidenService itemsService) {
+        this.AidenService = itemsService;
+    }
+    
 }
--- src/main/java/org/factoriaf5/game/services/AidenService.java
+++ src/main/java/org/factoriaf5/game/services/AidenService.java
@@ -1,5 +1,24 @@
 package org.factoriaf5.game.services;
 
-public class AidenService {
 
+    
+import java.util.List;
+
+import org.factoriaf5.game.models.Aiden;
+import org.factoriaf5.game.repositories.AidenRepository;
+import org.springframework.stereotype.Service;
+@Service
+public class AidenService{
+   
+    AidenRepository repository;
+
+    public AidenService(AidenRepository repository) {
+        this.repository = repository;
+    }
+    
+      public List<Aiden> getAll(){
+        List<Aiden> aiden = repository.findAll();
+      
+        return aiden;
+    }
 }
