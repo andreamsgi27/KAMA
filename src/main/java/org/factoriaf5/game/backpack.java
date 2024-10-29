14b3398546269fffb9c35928e67cd616c8967f97
andreamsgi27 <andreamsgi27@hotmail.com>
2024-10-29 15:04:48 +0100

    changes aproved in backpack

--- src/main/java/org/factoriaf5/game/backpack.java
+++ src/main/java/org/factoriaf5/game/backpack.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Backpack {
+public class Backpack {/* 
     private List<Items> itemsList;  
     private final int backpackCapacity = 3;
     private Scanner scanner;
