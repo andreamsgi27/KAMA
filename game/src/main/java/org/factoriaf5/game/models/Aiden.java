18300bdb6a246238bdc4ae4f682ce3570ec19003
mercyluz <mercyluz@hotmail.com>
2024-10-29 12:16:01 +0100

    creando aiden controller, model,service , repository

---
+++ game/src/main/java/org/factoriaf5/game/models/Aiden.java
@@ -0,0 +1,27 @@
+package org.factoriaf5.game.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "Aiden");
+public class Aiden {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String habilities;
+    private Long  Health;
+    private Long  Damage;
+
+    public Aiden(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    
+
+}
