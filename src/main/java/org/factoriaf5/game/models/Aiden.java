3ab17467512eebbed4e69795a485486f838c431e
mercyluz <mercyluz@hotmail.com>
2024-10-30 15:10:20 +0100

    agregando campos aiden

--- src/main/java/org/factoriaf5/game/models/Aiden.java
+++ src/main/java/org/factoriaf5/game/models/Aiden.java
@@ -12,18 +12,34 @@ public class Aiden {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String habilities;
+    private String aidenname;
+    private String aidendescription;
+    private String aidenhabilities;
     private int  aidenHealth;
     private int  aidenDamage;
 
     public Aiden(Long id, String name) {
         this.id = id;
-        this.name = name;
-
+        this.aidenname = name;
     }
-   
-public Aiden(){
+
+        public Long getId() {
+            return id;
+        }
+    
+        public String getItemName() {
+            return aidenname;
+        }
+    
+        public void setItemName(String itemName) {
+            this.aidenname = itemName;
+        }
+    
+        public String getItemDescription() {
+            return aidendescription;
+        }
+  
+    public Aiden(){
         this.aidenHealth = 100; 
         this.aidenDamage = 15;
 }
@@ -40,51 +56,4 @@ public Aiden(){
     public int getAidenDamage() {
         return aidenDamage;
     }
-
-    public void setAidenDamage(int aidenDamage) {
-        this.aidenDamage = aidenDamage;
-    }
-//public void recibir daño y habilidades
-public void receiveDamage(int monsterDamage) {
-    setAidenHealth(getAidenHealth() - monsterDamage);
 }
-public void powerStrike() {
-    aidenDamage += 10;
-}
-
-public int shield() {
-
-    int monsterAttack = Monster.getMonsterAttack();
-    int reducedAttack = monsterAttack - 5;
-    reducedAttack = reducedAttack > 0 ? reducedAttack : 0;
-    Monster.setMonsterAttack(reducedAttack);
-    return reducedAttack;   
-           
-    }
-    //aumentar vidas
-    public void incrementHealth(int bonus) {
-        setAidenHealth(getAidenHealth() + bonus);
-    
-    }
-    //hacer un boolean para saber si aiden esta vivo o si esta muerto
-    public boolean aidenisAlive() {
-        return getAidenHealth() > 0;
-    }
-    //hacer un metodo que imprima cuando aide esta muerto y sacar estadisticas
-
-     public void aidenDie() {
-    if (getAidenHealth() <= 0);
-        System.out.println("Aiden ha muerto.");
-        System.out.println(stat());
-     }
-    
-    public String stat() {
-        return "Estadísticas de la partida:\n" +  
-                "Puntuación: " + getscore() + "\n" +
-                "Nivel que ha muerto: " + getlevels() + "\n" +
-                "Contador de eventos: " + geteventcont();
-  }
-    
-}
-
-
