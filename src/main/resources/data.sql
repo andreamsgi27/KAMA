6e48c2b5cc0d10256b50b1be003e01c1a0aca578
andreamsgi27 <andreamsgi27@hotmail.com>
2024-11-04 21:56:43 +0100

    "Reformatted INSERT INTO monsters statement"

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,11 +7,9 @@ INSERT INTO games (game_date, player_name, game_cleared, final_score) VALUES ('2
 INSERT INTO games (game_date, player_name, game_cleared, final_score) VALUES ('2024-10-16', 'Mercy', FALSE, 430);
 INSERT INTO games (game_date, player_name, game_cleared, final_score) VALUES ('2024-10-13', 'Arancha', FALSE, 220);
 INSERT INTO games (game_date, player_name, game_cleared, final_score) VALUES ('2024-10-15', 'Anaís', FALSE, 390);
-INSERT INTO games (game_date, player_name, game_cleared, final_score) VALUES ('2024-10-14', 'Esmeralda', TRUE, 22250);  
-insert into monsters (monsterDamage, monsterHealth, monsterName, bonus) 
-values(
-    5,'Esqueleto',50,'Esqueleto',10;
-    7,'Vampiro',60,'Vampiro',15;
-    10,'Fantasma',70,'Fantasma',20;
+INSERT INTO games (game_date, player_name, game_cleared, final_score) VALUES ('2024-10-14', 'Esmeralda', TRUE, 22250);
 
-)
+INSERT INTO monsters (monsterDamage, monsterName, monsterHealth, bonus) VALUES
+(5, 'Esqueleto', 50, 10),
+(7, 'Vampiro', 60, 15),
+(10, 'Fantasma', 70, 20);
