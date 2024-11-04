3d25c02be9baa3572b16b5eb429b69c600784a08
andreamsgi27 <andreamsgi27@hotmail.com>
2024-11-04 20:51:25 +0100

    "Updated Spring Boot version to 3.3.5, removed Lombok annotation in Backpack.java, and added getter/setter methods. Also, removed unnecessary lines and imports in other model classes."

--- src/main/java/org/factoriaf5/game/models/Backpack.java
+++ src/main/java/org/factoriaf5/game/models/Backpack.java
@@ -1,9 +1,6 @@
 package org.factoriaf5.game.models;
 
 import jakarta.persistence.*;
-import lombok.Data;
-
-@Data
 @Entity
 @Table(name = "backpack")
 public class Backpack {
@@ -32,6 +29,46 @@ public class Backpack {
     // Constructor con parámetros
     public Backpack(Long id, String name) {
         this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Items getItem1() {
+        return item1;
+    }
+
+    public void setItem1(Items item1) {
+        this.item1 = item1;
+    }
+
+    public Items getItem2() {
+        return item2;
+    }
+
+    public void setItem2(Items item2) {
+        this.item2 = item2;
+    }
+
+    public Items getItem3() {
+        return item3;
+    }
+
+    public void setItem3(Items item3) {
+        this.item3 = item3;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 }
--- src/main/java/org/factoriaf5/game/models/Game.java
+++ src/main/java/org/factoriaf5/game/models/Game.java
@@ -1,4 +1,3 @@
-
 package org.factoriaf5.game.models;
 
 
