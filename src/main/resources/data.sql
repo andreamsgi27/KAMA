fecd640d004a88d5d9dc74a6491746e9f829fa1f
mercyluz <mercyluz@hotmail.com>
2024-11-05 11:36:18 +0100

    insertando tabla aiden, items

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,12 @@ INSERT INTO monsters (id_monster, type_monster, monster_damage, monster_health, 
 VALUES (1, 'Esqueleto', 5, 50, 'Esqueleto', 10, NULL, NULL, 5),
        (2, 'Vampiro', 7, 60, 'Vampiro', 15, TRUE, NULL, NULL),
        (3, 'Fantasma', 10, 70, 'Fantasma', 20, NULL, TRUE, NULL);
+
+INSERT INTO items (item_name, item_description) VALUES 
+('Lanza', 'Una lanza afilada, útil para el combate.'), 
+('Poción', 'Una poción curativa que restaura salud.'), 
+('Ajo', 'Un diente de ajo, eficaz contra los vampiros.'), 
+('Gafas', 'Unas gafas que permiten ver a los fantasmas.'), 
+('Silbato', 'Un silbato que puede atraer a un lobo amigo.');
+INSERT INTO Aiden(id, aiden_Name, aiden_Description, aiden_Ability, aiden_Health, aiden_Damage)
+VALUES (1, 'AIDEN', 'Es un heroe que posee habilidades cuya finalidad es derrotar al mago Mortis', 'Golpe potente y escudo protector', 100,15);
