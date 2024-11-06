c373592074a43de68322efaa7ec0a8512b4e0bdd
andreamsgi27 <andreamsgi27@hotmail.com>
2024-11-06 12:55:35 +0100

    "Added setter for id in MonsterModel and modified ItemsServicesTest to include new test methods for spear, potion, and glasses."

--- src/test/java/org/factoriaf5/game/services/ItemsServicesTest.java
+++ src/test/java/org/factoriaf5/game/services/ItemsServicesTest.java
@@ -2,10 +2,14 @@ package org.factoriaf5.game.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 import org.factoriaf5.game.models.Items;
+import org.factoriaf5.game.models.MonsterModel;
 import org.factoriaf5.game.repositories.ItemsRepository;
+import org.factoriaf5.game.repositories.MonsterRepository;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
@@ -26,10 +30,9 @@ public class ItemsServicesTest {
 
     @Mock
     ItemsRepository repository;
-
+    
     @Mock
-    private Random random;
-    //hay que mockear el random en los tests
+    private MonsterRepository mrepository;
 
     @Test
     void testgetAllItems(){
@@ -56,4 +59,37 @@ public class ItemsServicesTest {
         assertThat(result.equals("Lanza") || result.equals("Poción") || result.equals("Ajo") || result.equals("Gafas") || result.equals("Silbato"), equalTo(true));
 
     }
+
+    @Test
+    void spear() {
+        int aidenDamage = 20;
+        int result = service.spear(aidenDamage);
+        assertThat(result, equalTo(30));
+    }
+
+    @Test
+    void potion(){
+        int aidenHealth = 10;
+        int result = service.potion(aidenHealth);
+        assertThat(result, equalTo(30));
+    }
+
+    @Test
+    void testGlasses() {
+    // Preparamos los datos
+    MonsterModel monster = new MonsterModel();
+    monster.setId(1L);
+    monster.setTypeMonster("Fantasma");
+    when(mrepository.findById(1L)).thenReturn(Optional.of(monster)); // Devuelves un Optional del monster
+
+    // Llamamos al método
+    boolean result = service.glasses(1L);
+
+    // Verificamos el resultado
+    assertThat(result, equalTo(true));
+    assertThat(monster.isInvisibleActive(), equalTo(false)); // Verificamos que la invisibilidad fue desactivada
+}
+
+
+
 }
