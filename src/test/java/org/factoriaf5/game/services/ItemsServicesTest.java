009a69ba57a2056d7f16e645785e9364f36d4688
andreamsgi27 <andreamsgi27@hotmail.com>
2024-11-06 12:14:32 +0100

    "Added import for Random, mocked Random in test class, and added new test method itemFound()"

--- src/test/java/org/factoriaf5/game/services/ItemsServicesTest.java
+++ src/test/java/org/factoriaf5/game/services/ItemsServicesTest.java
@@ -2,6 +2,7 @@ package org.factoriaf5.game.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.factoriaf5.game.models.Items;
 import org.factoriaf5.game.repositories.ItemsRepository;
@@ -14,7 +15,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ItemsServicesTest {
@@ -24,6 +26,10 @@ public class ItemsServicesTest {
 
     @Mock
     ItemsRepository repository;
+
+    @Mock
+    private Random random;
+    //hay que mockear el random en los tests
 
     @Test
     void testgetAllItems(){
@@ -41,6 +47,13 @@ public class ItemsServicesTest {
         assertThat(result.get(0).getItemName(), equalTo("Poción"));
         assertThat(result, contains(potion, spear));
 
+    }
+
+    @Test
+    void itemFound() {
+        String result = service.itemFound();
+
+        assertThat(result.equals("Lanza") || result.equals("Poción") || result.equals("Ajo") || result.equals("Gafas") || result.equals("Silbato"), equalTo(true));
 
     }
 }
